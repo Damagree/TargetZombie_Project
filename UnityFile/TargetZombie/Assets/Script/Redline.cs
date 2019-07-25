9727226e9b21ab13b97f45d2022869b12cd54f55
Damagree <aryo.damar01@gmail.com>
2019-07-25 17:30:41 +0700

    - Modified field Arena * fixing behavior

---
+++ UnityFile/TargetZombie/Assets/Script/Redline.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Redline : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Civilian")
+        {
+            PlayerStats.Score++;
+            Debug.Log("Saved");
+        }
+    }
+}
